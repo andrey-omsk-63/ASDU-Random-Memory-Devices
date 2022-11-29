61723152cffb5974dacec7368d767d8f9ebeb37b
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-11-29 20:24:58 +0600

    '29112024'

--- src/components/MainMapStyle.ts
+++ src/components/MainMapStyle.ts
@@ -1,7 +1,7 @@
 export const searchControl = {
-  float: "left",
-  provider: "yandex#search",
-  size: "large",
+  float: 'left',
+  provider: 'yandex#search',
+  size: 'large',
 };
 
 export const styleInfoSoob = {
@@ -9,69 +9,135 @@ export const styleInfoSoob = {
   border: 0,
   marginRight: 0.1,
   width: 650,
-  maxHeight: "21px",
-  minHeight: "21px",
-  backgroundColor: "#E9F5D8",
-  color: "#E6761B",
-  textTransform: "unset !important",
+  maxHeight: '21px',
+  minHeight: '21px',
+  backgroundColor: '#E9F5D8',
+  color: '#E6761B',
+  textTransform: 'unset !important',
 };
 
 export const styleModalEnd = {
-  position: "absolute",
-  top: "0%",
-  left: "auto",
-  right: "-0%",
-  height: "21px",
-  maxWidth: "2%",
-  minWidth: "2%",
-  color: "black",
+  position: 'absolute',
+  top: '0%',
+  left: 'auto',
+  right: '-0%',
+  height: '21px',
+  maxWidth: '2%',
+  minWidth: '2%',
+  color: 'black',
 };
 
 export const styleSetInf = {
-  position: "absolute",
-  marginTop: "15vh",
-  marginLeft: "24vh",
+  position: 'absolute',
+  marginTop: '15vh',
+  marginLeft: '24vh',
   width: 380,
-  bgcolor: "background.paper",
-  border: "3px solid #000",
-  borderColor: "primary.main",
+  bgcolor: 'background.paper',
+  border: '3px solid #000',
+  borderColor: 'primary.main',
   borderRadius: 2,
   boxShadow: 24,
   p: 1.5,
 };
 //=== InputObject =====================================================
 export const styleSet = {
-  width: "230px",
-  maxHeight: "4px",
-  minHeight: "4px",
-  bgcolor: "#FAFAFA",
+  //marginTop: '2vh',
+  width: '230px',
+  maxHeight: '4px',
+  minHeight: '4px',
+  bgcolor: '#FAFAFA',
   boxShadow: 3,
-  textAlign: "center",
+  textAlign: 'center',
   p: 1.5,
 };
 
 export const styleInpKnop = {
   fontSize: 13.3,
-  color: "black",
-  maxHeight: "27px",
-  minHeight: "27px",
-  maxWidth: "62px",
-  minWidth: "62px",
-  backgroundColor: "#FFDB4D",
-  textTransform: "unset !important",
+  color: 'black',
+  maxHeight: '27px',
+  minHeight: '27px',
+  maxWidth: '62px',
+  minWidth: '62px',
+  backgroundColor: '#FFDB4D',
+  textTransform: 'unset !important',
 };
 
 export const styleSetAdress = {
-  width: "319px",
-  height: "30px",
-  marginTop: "9vh",
-  marginLeft: "48px",
+  outline: 'none',
+  width: '319px',
+  //width: '309px',
+  height: '160px',
+  marginTop: '10vh',
+  marginLeft: '48px',
+  //border: 1,
+  bgcolor: '#FFFEF7',
+  opacity: 0.8,
 };
 
 export const styleBoxForm = {
-  "& > :not(style)": {
-    marginTop: "-9px",
-    marginLeft: "-12px",
-    width: "255px",
+  '& > :not(style)': {
+    marginTop: '-9px',
+    marginLeft: '-12px',
+    width: '255px',
+  },
+};
+//=== InputVertex =====================================================
+export const styleSetAdrAreaID = {
+  //border: 1,
+  marginTop: '10vh',
+  marginLeft: '48px',
+  width: '316px',
+  height: '14vh',
+  // bgcolor: "#FAFAFA",
+  // bgcolor: "#DAF0C5",
+  bgcolor: '#FFFEF7',
+  opacity: 0.8,
+};
+
+export const styleSetAdrArea = {
+  marginTop: '3vh',
+  width: '319px',
+  height: '30px',
+};
+
+export const styleSetAdrID = {
+  width: '319px',
+  height: '30px',
+  //marginTop: '-1vh',
+};
+
+export const styleSetArea = {
+  width: '230px',
+  maxHeight: '3px',
+  minHeight: '3px',
+  bgcolor: '#FAFAFA',
+  boxShadow: 3,
+  textAlign: 'center',
+  p: 1.5,
+};
+
+export const styleSetID = {
+  width: '230px',
+  maxHeight: '3px',
+  minHeight: '3px',
+  bgcolor: '#FAFAFA',
+  boxShadow: 3,
+  textAlign: 'center',
+  p: 1.5,
+};
+
+export const styleBoxFormArea = {
+  '& > :not(style)': {
+    marginTop: '-10px',
+    marginLeft: '-12px',
+    width: '253px',
+  },
+};
+
+export const styleBoxFormID = {
+  '& > :not(style)': {
+    marginTop: '-9px',
+    marginLeft: '-12px',
+    width: '253px',
   },
 };
--- src/components/RgsComponents/RgsCreateObject.tsx
+++ src/components/RgsComponents/RgsCreateObject.tsx
@@ -1,17 +1,23 @@
-import * as React from "react";
-//import { useDispatch, useSelector } from "react-redux";
+import * as React from 'react';
+import { useSelector } from 'react-redux';
 //import { massdkCreate, massrouteCreate } from "../../redux/actions";
 
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
 
 //import { MapssdkNewPoint, MassrouteNewPoint } from "../MapServiceFunctions";
 
-import { styleSetAdress, styleBoxForm, styleInpKnop } from "../MainMapStyle";
-import { styleSet } from "../MainMapStyle";
+import { styleSetAdress, styleBoxForm, styleInpKnop } from '../MainMapStyle';
+import { styleSet } from '../MainMapStyle';
+
+import { styleSetAdrAreaID } from './../MainMapStyle';
+import { styleSetAdrArea, styleSetAdrID } from './../MainMapStyle';
+import { styleSetArea, styleSetID } from './../MainMapStyle';
+import { styleBoxFormArea, styleBoxFormID } from './../MainMapStyle';
 
 let chNewCoord = 1;
 
@@ -21,6 +27,14 @@ const RgsCreateObject = (props: {
   //createPoint: any;
 }) => {
   //== Piece of Redux ======================================
+  const map = useSelector((state: any) => {
+    const { mapReducer } = state;
+    return mapReducer.map.dateMap;
+  });
+  let datestat = useSelector((state: any) => {
+    const { statsaveReducer } = state;
+    return statsaveReducer.datestat;
+  });
   // let massdk = useSelector((state: any) => {
   //   const { massdkReducer } = state;
   //   return massdkReducer.massdk;
@@ -31,60 +45,56 @@ const RgsCreateObject = (props: {
   // });
   // const dispatch = useDispatch();
   //========================================================
+  let homeRegion = map.regionInfo[datestat.region];
+  let dat = map.areaInfo[homeRegion];
+  let massKey = [];
+  let massDat = [];
+  const currencies: any = [];
+  for (let key in dat) {
+    massKey.push(key);
+    massDat.push(dat[key]);
+  }
+  for (let i = 0; i < massKey.length; i++) {
+    let maskCurrencies = {
+      value: '',
+      label: '',
+    };
+    maskCurrencies.value = massKey[i];
+    maskCurrencies.label = massDat[i];
+    currencies.push(maskCurrencies);
+  }
+
   const [openSetAdress, setOpenSetAdress] = React.useState(true);
-  const [valuen, setValuen] = React.useState(
-    "Объект 1000" + String(chNewCoord)
-  );
-
-  const styleSet = {
-    width: "230px",
-    maxHeight: "4px",
-    minHeight: "4px",
-    bgcolor: "#FAFAFA",
-    boxShadow: 3,
-    textAlign: "center",
-    p: 1.5,
-  };
-
-  const styleInpKnop = {
-    fontSize: 13.3,
-    color: "black",
-    maxHeight: "27px",
-    minHeight: "27px",
-    maxWidth: "62px",
-    minWidth: "62px",
-    backgroundColor: "#FFDB4D",
-    textTransform: "unset !important",
-  };
-
-  const styleSetAdress = {
-    
-    width: "319px",
-    height: "230px",
-    marginTop: "9vh",
-    marginLeft: "48px",
-    bgcolor: "#FFFEF7",
-    opacity: 0.8,
-  };
-
-  const styleBoxForm = {
-    "& > :not(style)": {
-      marginTop: "-9px",
-      marginLeft: "-12px",
-      width: "255px",
-    },
-  };
+  const [valuen1, setValuen1] = React.useState('Объект 1000' + String(chNewCoord));
+  const [currency, setCurrency] = React.useState(massKey[0]);
+  const [valuen3, setValuen3] = React.useState(10000);
+  const [openSetErr, setOpenSetErr] = React.useState(false);
 
   const handleKey = (event: any) => {
-    if (event.key === "Enter") event.preventDefault();
+    if (event.key === 'Enter') event.preventDefault();
   };
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValuen(event.target.value.trimStart()); // удаление пробелов в начале строки
+  const handleChange1 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValuen1(event.target.value.trimStart()); // удаление пробелов в начале строки
     setOpenSetAdress(true);
   };
 
+  const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+    console.log('setCurrency:', currency);
+    setOpenSetAdress(true);
+  };
+
+  const handleChangeID = (event: any) => {
+    let valueInp = event.target.value.replace(/^0+/, '');
+    if (Number(valueInp) < 10000) valueInp = 10000;
+    if (valueInp === '') valueInp = 0;
+    valueInp = Math.trunc(Number(valueInp)).toString();
+    setValuen3(valueInp);
+  };
+
   const handleCloseSetAdr = () => {
+    props.setOpen(false);
     // massdk.push(MapssdkNewPoint(props.region, props.coord, valuen, 0, 0));
     // massroute.vertexes.push(
     //   MassrouteNewPoint(props.region, props.coord, valuen, 0, 0)
@@ -100,9 +110,62 @@ const RgsCreateObject = (props: {
     setOpenSetAdress(false);
   };
 
+  const InputArea = () => {
+    return (
+      <Box sx={styleSetArea}>
+        <Box component="form" sx={styleBoxFormArea}>
+          <TextField
+            select
+            size="small"
+            onKeyPress={handleKey} //отключение Enter
+            value={currency}
+            onChange={handleChange2}
+            InputProps={{ style: { fontSize: 13.5 } }}
+            variant="standard"
+            helperText="Введите район"
+            color="secondary">
+            {currencies.map((option: any) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Box>
+      </Box>
+    );
+  };
+
+  //const handleClose = () => {
+  // if (CheckAvailVertex()) {
+  //   if (CheckDoublAreaID()) {
+  //     massdk.push(
+  //       MapssdkNewPoint(
+  //         props.region,
+  //         props.coord,
+  //         adrV,
+  //         Number(currency),
+  //         Number(valuen)
+  //       )
+  //     );
+  //     massroute.vertexes.push(
+  //       MassrouteNewPoint(
+  //         props.region,
+  //         props.coord,
+  //         adrV,
+  //         Number(currency),
+  //         Number(valuen)
+  //       )
+  //     );
+  //     dispatch(massdkCreate(massdk));
+  //     dispatch(massrouteCreate(massroute));
+  //setOpenSetAdress(false);
+  //props.createPoint(props.coord);
+  // }
+  //}
+  //};
+
   return (
     <Modal open={openSetAdress} onClose={handleCloseSetAdress} hideBackdrop>
-   
       <Grid item container sx={styleSetAdress}>
         <Grid item xs={9.5} sx={{ border: 0 }}>
           <Box sx={styleSet}>
@@ -111,22 +174,53 @@ const RgsCreateObject = (props: {
                 size="small"
                 onKeyPress={handleKey} //отключение Enter
                 inputProps={{ style: { fontSize: 13.3 } }}
-                value={valuen}
-                onChange={handleChange}
+                value={valuen1}
+                onChange={handleChange1}
                 variant="standard"
                 helperText="Введите наименование объекта"
                 color="secondary"
               />
             </Box>
           </Box>
+          <Grid item container sx={styleSetAdrArea}>
+            <Grid item xs={9.5}>
+              <InputArea />
+            </Grid>
+          </Grid>
         </Grid>
-        <Grid item xs sx={{ border: 0 }}>
+
+        <Grid item container sx={styleSetAdrID}>
+          <Grid item xs={9.5}>
+            <Box sx={styleSetID}>
+              <Box component="form" sx={styleBoxFormID}>
+                <TextField
+                  size="small"
+                  onKeyPress={handleKey} //отключение Enter
+                  type="number"
+                  inputProps={{ style: { fontSize: 13.3 } }}
+                  value={valuen3}
+                  onChange={handleChangeID}
+                  variant="standard"
+                  helperText="Введите ID"
+                  color="secondary"
+                />
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs>
+            <Button sx={styleInpKnop} onClick={handleCloseSetAdr}>
+              Ввод
+            </Button>
+          </Grid>
+        </Grid>
+
+        {/* <Grid item xs sx={{ border: 0 }}>
           <Box>
             <Button sx={styleInpKnop} onClick={handleCloseSetAdr}>
               Ввод
             </Button>
           </Box>
-        </Grid>
+        </Grid> */}
       </Grid>
     </Modal>
   );
