ab654b9c92ca07e2540eec070069e150bc4fe287
andrey-omsk-63 <andrey_omsk@mail.ru>
2022-12-05 23:04:00 +0600

    '05122303'

--- src/components/RgsComponents/RgsAppointVertex.tsx
+++ src/components/RgsComponents/RgsAppointVertex.tsx
@@ -1,18 +1,18 @@
-import * as React from "react";
+import * as React from 'react';
 //import { useDispatch, useSelector } from 'react-redux';
 //import { addobjCreate, coordinatesCreate } from '../../redux/actions';
 
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Modal from "@mui/material/Modal";
-import TextField from "@mui/material/TextField";
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
 
 //import RgsEditName from './RgsEditName';
 
 //import { SendSocketDeleteAddObj } from '../MapSocketFunctions';
 
-import { styleModalEnd, styleModalMenu } from "../MainMapStyle";
+import { styleModalEnd, styleModalMenu } from '../MainMapStyle';
 //import { styleModalMenu } from '../MainMapStyle';
 
 const RgsAppointVertex = (props: {
@@ -43,15 +43,20 @@ const RgsAppointVertex = (props: {
   //========================================================
   const [openSet, setOpenSet] = React.useState(true);
   const [valueAreaZ, setValueAreaZ] = React.useState(10);
-  const [valueAreaS, setValueAreaS] = React.useState(0);
-  const [valueAreaV, setValueAreaV] = React.useState(0);
-  const [valueAreaU, setValueAreaU] = React.useState(0);
+  const [valueAreaS, setValueAreaS] = React.useState(7);
+  const [valueAreaV, setValueAreaV] = React.useState(1);
+  const [valueAreaU, setValueAreaU] = React.useState(2);
+  const [valueIdZ, setValueIdZ] = React.useState(12);
+  const [valueIdS, setValueIdS] = React.useState(17);
+  const [valueIdV, setValueIdV] = React.useState(43);
+  const [valueIdU, setValueIdU] = React.useState(65);
   //const [openProcess, setOpenProcess] = React.useState(false);
   //let idxObj = props.idx - map.tflight.length;
-  let heightBlock = window.innerWidth / 3 + 33;
+  //let heightBlock = window.innerWidth / 3 + 33;
+  let heightBlock = window.innerWidth / 3 + 12;
 
   const handleKey = (event: any) => {
-    if (event.key === "Enter") event.preventDefault();
+    if (event.key === 'Enter') event.preventDefault();
   };
 
   const handleCloseSet = () => {
@@ -63,27 +68,30 @@ const RgsAppointVertex = (props: {
     handleCloseSet();
   };
 
-  const handleChangeAreaZ = (event: any) => {
-    let valueInp = event.target.value.replace(/^0+/, "");
-    setValueAreaZ(valueInp);
+  const ChangeArea = (event: any, func: Function) => {
+    let valueInp = event.target.value.replace(/^0+/, '');
+    if (valueInp === '') valueInp = 1;
+    if (Number(valueInp) < 0) valueInp = 1;
+    if (Number(valueInp) < 100) func(valueInp);
   };
 
-  const handleChangeAreaS = () => {};
-
-  const handleChangeAreaV = () => {};
-
-  const handleChangeAreaU = () => {};
+  const ChangeId = (event: any, func: Function) => {
+    let valueInp = event.target.value.replace(/^0+/, '');
+    if (valueInp === '') valueInp = 1;
+    if (Number(valueInp) < 0) valueInp = 1;
+    if (Number(valueInp) < 1000) func(valueInp);
+  };
 
   const styleSetAppoint = {
-    outline: "none",
-    position: "relative",
-    marginTop: "12vh",
+    outline: 'none',
+    position: 'relative',
+    marginTop: '12vh',
     marginLeft: 1,
-    marginRight: "auto",
-    width: "98%",
-    bgcolor: "background.paper",
-    border: "3px solid #000",
-    borderColor: "primary.main",
+    marginRight: 'auto',
+    width: '98.5%',
+    bgcolor: 'background.paper',
+    border: '3px solid #000',
+    borderColor: 'primary.main',
     borderRadius: 2,
     boxShadow: 24,
   };
@@ -92,29 +100,29 @@ const RgsAppointVertex = (props: {
     //border: 1,
     height: heightBlock / 5,
     paddingTop: 3,
-    textAlign: "center",
+    textAlign: 'center',
   };
 
   const styleAppSt01 = {
     border: 0,
-    textAlign: "center",
+    textAlign: 'center',
     height: heightBlock / 15,
   };
 
   const styleAppSt02 = {
     //border: 1,
-    transform: "translate(-50%, -50%)",
-    position: "relative",
-    top: "50%",
-    left: "50%",
+    transform: 'translate(-50%, -50%)',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
   };
 
   const styleAppSt03 = {
     //border: 1,
-    transform: "translate(-50%, -100%)",
-    position: "relative",
-    top: "50%",
-    left: "50%",
+    transform: 'translate(-50%, -100%)',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
   };
 
   const styleAppSt04 = {
@@ -122,27 +130,51 @@ const RgsAppointVertex = (props: {
     height: heightBlock / 15,
   };
 
-  const styleSetArea = {
-    width: "15px",
-    maxHeight: "6px",
-    minHeight: "6px",
-    bgcolor: "#FFFBE5",
+  const styleSet = {
+    width: '20px',
+    maxHeight: '6px',
+    minHeight: '6px',
+    bgcolor: '#FFFBE5',
     boxShadow: 3,
-    textAlign: "center",
+    textAlign: 'center',
     p: 1,
   };
 
-  const styleBoxFormArea = {
-    "& > :not(style)": {
-      marginTop: "-9px",
-      marginLeft: "-12px",
-      width: "33px",
+  const styleBoxForm = {
+    '& > :not(style)': {
+      marginTop: '-9px',
+      marginLeft: '-8px',
+      width: '35px',
     },
   };
 
+  // const styleSetId = {
+  //   // width: '36px',
+  //   // maxHeight: '21px',
+  //   // minHeight: '21px',
+  //   width: '24px',
+  //   maxHeight: '6px',
+  //   minHeight: '6px',
+  //   bgcolor: '#FFFBE5',
+  //   boxShadow: 3,
+  //   textAlign: 'center',
+  //   p: 1,
+  // };
+
+  // const styleBoxFormId = {
+  //   '& > :not(style)': {
+  //     // marginTop: '-2px',
+  //     // marginLeft: '-2px',
+  //     // width: '38px',
+  //     marginTop: '-9px',
+  //     marginLeft: '-8px',
+  //     width: '38px',
+  //   },
+  // };
+
   const AppointHeader = () => {
     return (
-      <Grid container sx={{ bgcolor: "#C0E2C3" }}>
+      <Grid container sx={{ bgcolor: '#C0E2C3' }}>
         <Grid item xs={1}></Grid>
         <Grid item xs={4.75} sx={styleAppHeader}>
           <Box sx={styleAppSt03}>
@@ -165,15 +197,15 @@ const RgsAppointVertex = (props: {
 
   const InputerArea = (value: any, func: any) => {
     return (
-      <Box sx={styleSetArea}>
-        <Box component="form" sx={styleBoxFormArea}>
+      <Box sx={styleSet}>
+        <Box component="form" sx={styleBoxForm}>
           <TextField
             size="small"
             type="number"
             onKeyPress={handleKey} //отключение Enter
-            value={valueAreaZ}
-            inputProps={{ style: { fontSize: 14.2 } }}
-            onChange={handleChangeAreaZ}
+            value={value}
+            inputProps={{ style: { fontSize: 13.2 } }}
+            onChange={(e) => ChangeArea(e, func)}
             variant="standard"
             color="secondary"
           />
@@ -182,7 +214,32 @@ const RgsAppointVertex = (props: {
     );
   };
 
-  const AppointStroka = (rec1: string) => {
+  const InputerId = (value: any, func: any) => {
+    return (
+      <Box sx={styleSet}>
+        <Box component="form" sx={styleBoxForm}>
+          <TextField
+            size="small"
+            type="number"
+            onKeyPress={handleKey} //отключение Enter
+            value={value}
+            inputProps={{ style: { fontSize: 13.2 } }}
+            onChange={(e) => ChangeId(e, func)}
+            variant="standard"
+            color="secondary"
+          />
+        </Box>
+      </Box>
+    );
+  };
+
+  const AppointStroka = (
+    rec1: string,
+    valueAr: any,
+    funcAr: Function,
+    valueId: any,
+    funcId: Function,
+  ) => {
     return (
       <Grid container sx={{ borderBottom: 1 }}>
         {/* === Направление === */}
@@ -200,21 +257,19 @@ const RgsAppointVertex = (props: {
         {/* === Откуда === */}
         <Grid item xs={4.75} sx={{ fontSize: 14, height: heightBlock / 5 }}>
           <Grid container>
-            <Grid item xs={8.8} sx={styleAppSt04}>
+            <Grid item xs={8.6} sx={styleAppSt04}>
               <Box sx={styleAppSt02}>Ведите район</Box>
             </Grid>
-            <Grid item xs sx={{ border: 1 }}>
-              <Box sx={styleAppSt02}>
-                {InputerArea(valueAreaZ, handleChangeAreaZ)}
-              </Box>
+            <Grid item xs sx={{ border: 0 }}>
+              <Box sx={styleAppSt02}>{InputerArea(valueAr, funcAr)}</Box>
             </Grid>
           </Grid>
           <Grid container>
-            <Grid item xs={10} sx={styleAppSt04}>
+            <Grid item xs={8.6} sx={styleAppSt04}>
               <Box sx={styleAppSt02}>Ведите ID</Box>
             </Grid>
-            <Grid item xs={2} sx={{ border: 0 }}>
-              <Box sx={styleAppSt02}>6</Box>
+            <Grid item xs sx={{ border: 0 }}>
+              <Box sx={styleAppSt02}>{InputerId(valueId, funcId)}</Box>
             </Grid>
           </Grid>
           <Grid container>
@@ -263,7 +318,7 @@ const RgsAppointVertex = (props: {
         <Button sx={styleModalEnd} onClick={handleCloseSet}>
           &#10006;
         </Button>
-        <Box sx={{ fontSize: 17, textAlign: "center" }}>
+        <Box sx={{ fontSize: 17, textAlign: 'center' }}>
           <b>Массив связности перекрёстка</b>
         </Box>
         <Grid container sx={{ marginTop: 0.5, paddingBottom: 1 }}>
@@ -271,15 +326,15 @@ const RgsAppointVertex = (props: {
 
           <Grid item xs={4} sx={{ border: 0 }}>
             {AppointHeader()}
-            {AppointStroka("З")}
-            {AppointStroka("С")}
-            {AppointStroka("В")}
-            {AppointStroka("Ю")}
+            {AppointStroka('З', valueAreaZ, setValueAreaZ, valueIdZ, setValueIdZ)}
+            {AppointStroka('С', valueAreaS, setValueAreaS, valueIdS, setValueIdS)}
+            {AppointStroka('В', valueAreaV, setValueAreaV, valueIdV, setValueIdV)}
+            {AppointStroka('Ю', valueAreaU, setValueAreaU, valueIdU, setValueIdU)}
           </Grid>
 
           <Grid item xs={4} sx={{ border: 0 }}></Grid>
         </Grid>
-        <Box sx={{ marginTop: 1, textAlign: "center" }}>
+        <Box sx={{ marginTop: 1, textAlign: 'center' }}>
           <Button sx={styleModalMenu} onClick={() => handleClose()}>
             Сохранить изменения
           </Button>
