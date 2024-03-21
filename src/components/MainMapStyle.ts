c5a9220823793c9391d5e155e8d4564cf8eb019a
andrey-omsk-63 <andrey_omsk@mail.ru>
2024-03-21 13:44:13 +0600

    2103/1344

--- src/components/MainMapStyle.ts
+++ src/components/MainMapStyle.ts
@@ -209,14 +209,13 @@ export const styleSetAppoint = {
   // marginRight: "auto",
   //===
   // position: 'absolute',
-   left: '50%',
-   top: '50%',
-   transform: 'translate(-50%, -50%)',
+  left: "50%",
+  top: "50%",
+  transform: "translate(-50%, -50%)",
   //===
   width: "98.5%",
   bgcolor: "background.paper",
   border: "1px solid #fff",
-  //borderColor: "primary.main",
   borderRadius: 1,
   boxShadow: 24,
 };
